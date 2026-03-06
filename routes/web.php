1e04965d23719f6b62e8831eaf82e555084d66d1
redDeadth <mimamamemima335@gmail.com>
2026-03-05 22:32:27 -0500

    Cambiar ruta raíz para que redirija a la encuesta

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('survey.index');
 });
 
 Route::get('/encuesta', [SurveyController::class, 'index'])->name('survey.index');
